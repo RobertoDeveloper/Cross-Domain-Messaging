cfeee1a6661b1594cea6263d363e2b5d08ccaa3f
Roberto Rolon <Roberto.Rolon@therigy.local>
2016-02-02 17:08:23 -0500

    Include Origin Check

--- receiver.js
+++ receiver.js
@@ -3,7 +3,7 @@
          
          function  getMessage(e) {
              //you can also validate the origin below -> e.origin === 'yourOrigin'
-             if(!e.data) return;
+             if(!e.data || e.origin !== 'http://yourUrl') return;
              
              //set any data you want to send back to sender
              var dataToSend = document.cookie;
@@ -12,5 +12,5 @@
              e.source.postMessage(dataToSend, e.origin);
          }
         (w.addEventListener) ? w.addEventListener('message', getMessage) : w.attachEvent('message', getMessage); 
-    }
+    } 
 })(window);
